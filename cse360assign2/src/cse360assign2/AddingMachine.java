67bc63d5f27f26f1035449c1034c4920a9c216b3
Kenny Le <kvle2@asu.edu>
2019-10-09 19:21:34 -0700

    Finish methods

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -1,7 +1,9 @@
 /**
  *Name: Kenny Le
  *Class ID: 70641
- *Assignment #1
+ *Assignment #2
+ *This file will add and subtract from the previous number
+ *You are allowed to clear your history and start again from 0
  */
 package cse360assign2;
 
@@ -9,28 +11,54 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String history;
 	
+	/**
+	 * This method instantiates the history String to 0
+	 */
 	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
+		history = "0";
 	}
 	
+	/**
+	 * This method returns the total value
+	 * @return	int	This returns the total value of all the operations
+	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
+	/**
+	 * This method adds to the total and saves value into history
+	 * @param value	is the parameter that will be added to the total
+	 */
 	public void add (int value) {
-		System.out.println("SUP diggy");
+		total += value;
+		history = history + " + " + value;
 	}
 	
+	/**
+	 * This method subtracts to the total and saves value into history
+	 * @param value is the parameter that will be subtracted from to total
+	 */
 	public void subtract (int value) {
-		
+		total -= value;
+		history = history + " - " + value;
 	}
 		
+	/**
+	 * This method will print the history of added and subtracted values
+	 * @return String This returns all the additions and subtractions
+	 */
 	public String toString () {
-		return "";
+		return history;
 	}
 
+	/**
+	 * This method resets history to 0 and total to 0
+	 */
 	public void clear() {
-	
+		history = "0";
+		total = 0;
 	}
 }
